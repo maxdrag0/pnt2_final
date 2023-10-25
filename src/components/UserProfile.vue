6faa9fc6df11fda6a9d9d1a17886136d0e160951
Pedro Gorla <pedrogorla15@gmail.com>
2023-10-24 22:24:01 -0300

    Se intentó vincular con mockAPI y con la geolocalización, cambios en la estetica en UserProfile, organización

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,85 +1,104 @@
 <template>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <router-link to="/">
-                    <img id="baston" src="../assets/images/baston.png" alt="Botón de Inicio" />
-                </router-link>
-            </a>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <router-link to="/">
+          <img id="baston" src="../assets/images/baston.png" alt="Botón de Inicio" />
+        </router-link>
+      </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    </div>
+  </nav>
 
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li>
-                        <a>
-                            <router-link to="/User">
-                                <img id="perfil" src="../assets/images/perfil.png" alt="Botón de PERFIL" />
-                            </router-link>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Mi Perfil
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <router-link class="dropdown-item" to="/RegisterForm">Mis Datos</router-link>
-                            </li>
-                            <li>
-                                <router-link class="dropdown-item" to="/RegisterForm">Certificaciones</router-link>
-                            </li>
-                            <li>
-                                <router-link class="dropdown-item" to="/RegisterForm">Mis Solicitudes</router-link>
-                            </li>
-                            <li>
-                                <router-link class="dropdown-item" to="/RegisterForm">Solicitudes Completadas</router-link>
-                            </li>
-                            <li>
-                                <router-link class="dropdown-item" to="/RegisterForm">Mis Retos</router-link>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
   <div>
-    <!-- NAVEGACION -->
-    <!-- Aquí iría tu código de navegación si lo necesitas -->
-
     <!-- PERFIL -->
     <div v-if="userProfile">
-      <body>
-        <div class="card" style="width: 18rem">
-          <img
-            class="card-img-top"
-            src="../assets/images/perfil.png"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <h5 class="card-title">{{ userProfile.userName }}</h5>
-            <p class="card-text">{{ userProfile.userDescription }}</p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-              Tipo de usuario: {{ userProfile.userType }}
-            </li>
-            <li class="list-group-item">Ciudad: {{ userProfile.userCity }}</li>
-            <li class="list-group-item">Valoracion:</li>
-          </ul>
-          <div class="card-body">
-            <a href="#" class="card-link">Crear solicitud</a>
-            <a href="#" class="card-link">Ver solicitudes</a>
-          </div>
-        </div>
-      </body>
+      <div class="card profile-card">
+        <!-- Contenido de la tarjeta del perfil aquí -->
+      </div>
     </div>
-    <div v-else>Cargando perfil...</div>
+
+    <!-- Secciones agregadas con contenido ejemplar -->
+
+    <!-- Mis Datos -->
+    <section id="mis-datos">
+      <h2>Mis Datos</h2>
+      <!-- Tabla o contenido de ejemplo para Mis Datos -->
+
+    </section>
+
+    <!-- Certificaciones -->
+    <section id="certificaciones">
+      <h2>Certificaciones</h2>
+      <table class="volunteer-table">
+        <thead>
+          <tr>
+            <th>Nombre de Certificación</th>
+            <th>Fecha</th>
+            <th>Institución</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Certificación de Primeros Auxilios</td>
+            <td>01/02/2023</td>
+            <td>CRUZ ROJA</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+
+    <!-- Mis Solicitudes -->
+    <section id="mis-solicitudes">
+      <h2>Mis Solicitudes</h2>
+      <table class="volunteer-table">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Actividad Solicitada</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>12/10/2023</td>
+            <td>Asistencia para ir de compras</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+
+    <!-- Mis Retos -->
+    <section id="mis-retos">
+      <h2>Mis Retos</h2>
+      <p>Lista o contenido que represente los retos o metas del usuario.</p>
+
+    </section>
+
+    <!-- Solicitudes Completadas (sin cambios) -->
+    <section id="solicitudes-completadas">
+      <h2>Solicitudes Completadas</h2>
+      <table class="volunteer-table">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Actividad</th>
+            <th>Persona Ayudada</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>10/10/2023</td>
+            <td>Acompañamiento a consulta médica</td>
+            <td>Señor Rodriguez</td>
+          </tr>
+          <tr>
+            <td>05/10/2023</td>
+            <td>Compras de supermercado</td>
+            <td>Señora Perez</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   </div>
 </template>
 
@@ -92,7 +111,7 @@ const userProfile = ref(null);
 const fetchUserProfile = async () => {
   const userId = $route.params.userId;
   try {
-    const response = await axios.get(`https://tu-url-o-ruta/api/${userId}`);
+    const response = await axios.get(`https://6525d5d667cfb1e59ce7b745.mockapi.io/${userId}`);
     userProfile.value = response.data;
   } catch (error) {
     console.error("Hubo un error al obtener el perfil del usuario:", error);
@@ -107,11 +126,45 @@ onErrorCaptured((error) => {
 </script>
 
 <style scoped>
-/* Tus estilos aquí */
+.profile-card {
+  margin-top: 20px;
+  width: 18rem;
+}
+
+.volunteer-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 20px 0;
+}
+
+.volunteer-table th,
+.volunteer-table td {
+  padding: 10px;
+  border: 1px solid rgb(183, 119, 243);
+  /* Nuevo color para las bordes */
+}
+
+.volunteer-table th {
+  background: rgb(183, 119, 243);
+  /* Nuevo color para el encabezado */
+  color: white;
+}
+
+.volunteer-table tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
 .body {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+section {
+  margin: 30px 0;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 }
 
 .card {
