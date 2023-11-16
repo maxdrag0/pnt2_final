56485972b21784b504d634561b4d1e741cf06a01
Pedro Gorla <pedrogorla15@gmail.com>
2023-11-16 14:28:14 -0300

    falta configuracion de mock api en CrearSolicitud para pasar dicho campo a solicitudes.vue al crear una solicitud

--- src/components/CrearSolicitud.vue
+++ src/components/CrearSolicitud.vue
@@ -145,7 +145,8 @@ const submitSolicitud = async () => {
       //NUEVO
       router.push('/Solicitudes');
       //FIN NUEVO
-      $router.push({ path: "/Solicitudes" });
+      // $router.push({ path: "/Solicitudes" });
+      $router.push({ name: 'Solicitud', params: { solicitudId: response.data.id } });
 
     }
   } catch (error) {
